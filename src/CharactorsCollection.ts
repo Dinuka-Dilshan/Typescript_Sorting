72da460eb1dea64a22b2f4c569862db5dedcf633
Dinuka-Dilshan <jadinukadilshan@gmail.com>
2022-08-12 17:05:27 +0530

    charactor collection

---
+++ src/CharactorsCollection.ts
@@ -0,0 +1,24 @@
+import { Sortable } from './Sorter';
+
+export class CharactorsCollection implements Sortable {
+  constructor(private data: string) {}
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  get values() {
+    return this.data;
+  }
+
+  compare = (leftIndex: number, rightIndex: number): boolean =>
+    this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase();
+
+  swap = (leftIndex: number, rightIndex: number): void => {
+    const characters = this.data.split('');
+    const temp = characters[leftIndex];
+    characters[leftIndex] = characters[rightIndex];
+    characters[rightIndex] = temp;
+    this.data = characters.join('');
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { CharactorsCollection } from './CharactorsCollection.js';
 import { NumbersCollection } from './NumbersCollection.js';
 import { Sorter } from './Sorter.js';
 
 const numbers = new NumbersCollection([8, 6, 4, 7, 5]);
-const sorter = new Sorter(numbers);
+const string = new CharactorsCollection('dasec')
+const sorter = new Sorter(string);
 sorter.sort();
-console.log(numbers.values);
+console.log(string.values);
